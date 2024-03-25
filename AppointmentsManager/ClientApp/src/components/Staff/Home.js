63dc7632d2828b6ea66c69add487f715bc464cc2
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-03-25 13:01:24 +0530

    initial

--- AppointmentsManager/ClientApp/src/components/Staff/Home.js
+++ AppointmentsManager/ClientApp/src/components/Staff/Home.js
@@ -175,7 +175,7 @@ export default function Home(props) {
                   <div className="column time">Time</div>
                   <div className="column ">-</div>
                   <div className="column endTime">End Time</div>
-          <div className="column academicStaff">Academic Staff Memberxxxx</div>
+          <div className="column academicStaff">Academic Staff Memberxxxxx</div>
    
       </div>
           </div>*/}
