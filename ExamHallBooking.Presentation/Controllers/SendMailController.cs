629d229728c2023a20922a20ec4cc53bfa4efd64
Kishara <kisharamanilka0.com>
2024-05-25 15:55:09 +0530

    Update of mail Sending.

---
+++ ExamHallBooking.Presentation/Controllers/SendMailController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MailSend_DotNETCore8WebAPI.Interfaces;
+using MailSend_DotNETCore8WebAPI.Models;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace MailSend_DotNETCore8WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendMailController : ControllerBase
+    {
+        private readonly IMailSendService _mailService;
+        private readonly ILogger<SendMailController> _logger;
+
+        public SendMailController(IMailSendService mailService, ILogger<SendMailController> logger)
+        {
+            _mailService = mailService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail([FromForm] MailSendRequest request)
+        {
+            try
+            {
+                if (request == null || string.IsNullOrEmpty(request.Body))
+                {
+                    _logger.LogError("Invalid mail request. The request or Body is null.");
+                    return BadRequest("Invalid mail request. The Body is required.");
+                }
+
+                await _mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while sending email.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error");
+            }
+        }
+    }
+}
---
+++ ExamHallBooking.Presentation/MailService/Models/MailSendRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace MailSend_DotNETCore8WebAPI.Models
+{
+    public class MailSendRequest
+    {
+        public string ToEmail { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+        public List<IFormFile> Attachments { get;  set; }
+    }
+}
