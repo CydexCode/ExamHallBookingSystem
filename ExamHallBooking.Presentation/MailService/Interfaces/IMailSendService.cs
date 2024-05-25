cf94c6c6e078f37e3926eb7851c562a00c7b6542
Kishara Manilka <kisharamanilka0@gmail.com>
2024-05-25 16:23:38 +0530

    Update mail sending service

--- ExamHallBooking.Presentation/MailService/Interfaces/IMailSendService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MailSend_DotNETCore8WebAPI.Models;
-
-namespace MailSend_DotNETCore8WebAPI.Interfaces
-{
-    public interface IMailSendService
-    {
-        public Task SendEmailAsync(MailSendRequest mailrequest);
-    }
-}
