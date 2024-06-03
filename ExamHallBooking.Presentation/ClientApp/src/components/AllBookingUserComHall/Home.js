45578e78230c5b0231751795f59cb29a9fd9600f
manoda98 <2020e147@eng.jfn.ac.lk>
2024-06-03 22:55:27 +0530

    resolve merge issues

--- ExamHallBooking.Presentation/ClientApp/src/components/AllBookingUserComHall/Home.js
+++ ExamHallBooking.Presentation/ClientApp/src/components/AllBookingUserComHall/Home.js
@@ -8,21 +8,15 @@ import New from "./New"
 import Appointment from "./Appointment"
 import NavBar from "../../NavBar/NavBar.js"; // Make sure the path is correct
 
-import "./AR-com-user.css"
 import "../../NavBar/NavBar.css"; // Import the CSS file for styling (create this file)
-import "../../custom.css";
-import ConfirmSignOut from "./ConfirmSignOut"; // Import the ConfirmSignOut component
-
+import "./comHome.css";
 import { getDefault, openModal, filter, getAppointments, notifyUser } from "./Lib"
 
 export default function Home(props) {
+  const [dataList, setDataList] = useState([]);
 
-  const [dataList, setDataList] = useState([])
-
-  const [refreshData, setRefreshData] = useState(0)
-  const [stateListener, setStateListener] = useState(0)
-  const [showConfirmSignOut, setShowConfirmSignOut] = useState(false);
-
+  const [refreshData, setRefreshData] = useState(0);
+  const [stateListener, setStateListener] = useState(0);
 
   const filterApp = (e) => {
     let name_ = e.target.name;
@@ -34,8 +28,8 @@ export default function Home(props) {
 
       if (name_ === "SpecifiedDate") {
         filter.SpecifiedDate = new Date(v_);
-        filter.StartDate = null
-        filter.EndDate = null
+        filter.StartDate = null;
+        filter.EndDate = null;
       }
 
       if (name_ === "SpecifiedTime") {
@@ -52,7 +46,7 @@ export default function Home(props) {
         const dayNum = sd_.getDay();
 
         if (v_ === "1") {
-          sd_.setDate(dayNum - 1)
+          sd_.setDate(dayNum - 1);
         }
 
         if (v_ === "2") {
@@ -73,33 +67,16 @@ export default function Home(props) {
 
         filter.StartDate = v_ === '4' ? null : sd_;
         filter.EndDate = v_ === '4' ? null : ed_;
-        filter.SpecifiedDate = null
+        filter.SpecifiedDate = null;
       }
-
     }
 
-    /*      // Handle "Done" button click
-          if (name_ === "Done") {
-              // Set filter for "Done" appointments
-              filter.Done = v_;
-              // Fetch data with filter
-              getAppointments(filter).then(r => {
-                  if (r.length < 1) {
-                      notifyUser("Filter result is empty!")
-                  }
-                  setDataList(r)
-              }).catch(e => console.log("Error getting data on filter: ", e))
-          }*/
-
-
-
     if (name_ === "period") {
-      // 1 = today, 2 = this week, 3 = last week
       let sd_ = new Date(), ed_ = new Date();
       const dayNum = sd_.getDay();
 
       if (v_ === "1") {
-        sd_.setDate(dayNum - 1)
+        sd_.setDate(dayNum - 1);
       }
 
       if (v_ === "2") {
@@ -121,13 +98,12 @@ export default function Home(props) {
       filter.StartDate = v_ === '4' ? null : sd_;
       filter.EndDate = v_ === '4' ? null : ed_;
       filter.SpecifiedDate = null;
-
     }
 
     if (name_ === "SpecifiedDate") {
       filter.SpecifiedDate = new Date(v_);
-      filter.StartDate = null
-      filter.EndDate = null
+      filter.StartDate = null;
+      filter.EndDate = null;
     }
 
     if (name_ === "SpecifiedTime") {
@@ -138,15 +114,13 @@ export default function Home(props) {
       filter.LevelOfImportance = Number(v_) === 9 ? null : Number(v_);
     }
 
-    // fetch data with filter
     getAppointments(filter).then(r => {
       if (r.length < 1) {
-        notifyUser("Filter result is empty!")
+        notifyUser("Filter result is empty!");
       }
-      setDataList(r)
-    }).catch(e => console.log("Error getting data on filter: ", e))
-  }
-
+      setDataList(r);
+    }).catch(e => console.log("Error getting data on filter: ", e));
+  };
 
   useEffect(() => {
     getDefault().then(data => {
@@ -156,178 +130,158 @@ export default function Home(props) {
 
   const handleBackClick = () => {
     window.location.href = '/selectHallPage';
-  };
+};
 
   const handleMainClick = () => {
     window.location.href = '/';
-  };
+};
 
-  const handleUserClick = () => {
+const handleUserClick = () => {
     window.location.href = '/login';
-  };
-  const handleAdminClick = () => {
+};
+const handleAdminClick = () => {
     window.location.href = '/adminLogin';
-  };
-  const handleSignOutClick = () => {
-    setShowConfirmSignOut(true);
-  };
-
-  const confirmSignOut = () => {
-    setShowConfirmSignOut(false);
+};
+const handleSignOutClick = () => {
     window.location.href = '/';
-  };
+};
 
-  const cancelSignOut = () => {
-    setShowConfirmSignOut(false);
-  };
   return (
 
     <div>
-      <NavBar
+    <NavBar
 
-        onBackClick={handleBackClick}
-        showBackButton={true}
-        onMainClick={handleMainClick}
-        onUserClick={handleUserClick}
+       onBackClick={handleBackClick}
+       showBackButton={true}
+       onMainClick={handleMainClick}
+       onUserClick={handleUserClick}
 
-        onAdminClick={handleAdminClick}
-        onSignOutClick={handleSignOutClick}
+       onAdminClick={handleAdminClick}
+       onSignOutClick={handleSignOutClick}
         showAdminUser={false} // Hide User and Admin
-        showSignOutButton={true}
-      />
-      <body>
+        showSignOutButton ={true}
+    />
 
+      <main>
 
-
-          <div className="centered-heading-container">
-            <h1>Computer Department Exam Hall Booking System</h1>
+          <br></br>
+          <br></br>
+          
+          <div className="centered-heading">
+              <h1>Computer Department All Exams</h1>
           </div>
-          <main>
-          <section className="filter">
-            <div>
+          <div className="button-container-com ">
+          <div className="add-btn row items-center content-center">
+    <Link to="/staffLogin" className="btn add">
+        End Exam Booking
+    </Link>
+      </div>
 
-              <Link className="btn add" to="/staffLogin">End Exam Booking</Link>
-              <br></br>
-              <br></br>
-              <br></br>
-              {/* <Link to="/user">Mid / Quiz Booking</Link>*/}
-            </div>
-
-
-            <div className="add-btn row items-center content-center">
-              <div className="btn add" onClick={() => openModal("new-modal")}>Mid/Quiz/Assignment Booking</div>
-            </div>
-
-            {/*   <br></br>
+          <div className="add-btn row items-center content-center">
+              <div className="btn add" onClick={() => openModal("new-modal")}>Mid/Quiz Booking</div>
+          </div>
+          </div>
+       {/*   <br></br>
           <div className="add-btn2 row items-center content-center">
               <div className="btn add" onClick={() => openModal("new-modal")}>End Exam</div>
           </div>*/}
 
-            {/* <div className="notifications spacer-20"></div> */}
-
-
-            <div className="modal-title">Filters</div>
-            <div className="row items-center filter-items">
-              <button className="me-15" onClick={() => window.location.reload()}>Clear Filters</button>
-              <div>
-                <label htmlFor="All_f">All Requests</label> <br />
-                <input type="checkbox" id="All_f" name="All" onChange={filterApp} />
-              </div>
-
-              <div>
-                <label htmlFor="Done_f">accepted</label> <br />
-                <input type="checkbox" id="Done_f" name="Done" onChange={filterApp} />
-              </div>
-
-
-              <div>
-                <label htmlFor="Deleted_f">Rejected</label> <br />
-                <input type="checkbox" id="Deleted_f" name="Deleted" onChange={filterApp} />
-              </div>
-              <div>
-                <label htmlFor="period">Period</label> <br />
-                <select name="period" id="period" defaultValue={"4"} onChange={filterApp}>
-                  <option value="5" disabled>Period</option>
-                  <option value="4" >Default</option>
-                  <option value="1">Today</option>
-                  <option value="2">This week</option>
-                  <option value="3">Last week</option>
-                </select>
-              </div>
-
-              <div>
-                <label htmlFor="SpecifiedDate">Specified Date</label> <br />
-                <input type="date" id="SpecifiedDate" name="SpecifiedDate" onChange={filterApp} />
-              </div>
-
-              <div>
-                <label htmlFor="SpecifiedTime">Specified Start Time</label> <br />
-                <input type="time" id="SpecifiedTime" name="SpecifiedTime" onChange={filterApp} />
-              </div>
-
-              <div>
-                <label htmlFor="LevelOfImportance_f">Exam Type</label> <br />
-                <select name="LevelOfImportance" id="LevelOfImportance_f" defaultValue={8} onChange={filterApp}>
-                  <option value={8} disabled>Exam Type</option>
-                  <option value={9} >Reset</option>
-
-                  <option value={3}>Assignment</option>
-                  <option value={2}>Quiz</option>
-                  <option value={1}>Mid Exam</option>
-                  <option value={0}>End Exam</option>
-                </select>
-              </div>
-            </div>
-          </section>
-
-          <div className="userPage">
-            <div className="row underline hdr">
-              <div className="column id">Booking No</div>
-              <div className="column examHall">Exam Hall</div>
-              <div className="column lectureName">Lecturer Email Address</div>
-              <div className="column numOfStudent">Number Of Student</div>
-              <div className="column year">Batch</div>
-              <div className="column semester">Semester</div>
-              <div className="column subject">Subject</div>
-              <div className="column importance">Exam Type</div>
-              <div className="column date">Date</div>
-              <div className="column time">Start Time</div>
-              <div className="column ">-</div>
-              <div className="column endTime">End Time</div>
-              <div className="column academicStaff">Academic Staff Member</div>
-
-            </div>
       
-          {
-            dataList.length === 0 ?
-              <div className="row mt-15 waiting"><div className="loading"></div></div> :
-              dataList.map(item => <Appointment item={item} key={item.id} stateListener={setStateListener} />)
-          }
+       <div className="table1">
 
-
-          <section>
-            <section className="modal new-modal hidden">
-              <New refreshApp={setRefreshData} />
-            </section>
-            <div className="block">
-              <section className="modal edit-modal hidden">
-                <Edit stateListener={stateListener} refreshApp={setRefreshData} />
-              </section>
-
-              <section className="modal delete-modal hidden">
-                <Delete stateListener={stateListener} refreshApp={setRefreshData} />
-              </section>
-            </div>
-          </section>
+      <section className="row justify-btw items-center filter">
+        <div className="modal-title">Filter</div>
+        <div className="row items-center filter-items">
+          <div>
+            <label htmlFor="All_f">All</label> <br />
+            <input type="checkbox" id="All_f" name="All" onChange={filterApp} />
           </div>
-        </main>
-        {showConfirmSignOut && (
-          <ConfirmSignOut
-            onConfirm={confirmSignOut}
-            onCancel={cancelSignOut}
-          />
-        )}
-      </body>
+          <div>
+            <label htmlFor="Done_f">Accepted</label> <br />
+            <input type="checkbox" id="Done_f" name="Done" onChange={filterApp} />
+          </div>
+          <div>
+            <label htmlFor="Deleted_f">Rejected</label> <br />
+            <input type="checkbox" id="Deleted_f" name="Deleted" onChange={filterApp} />
+          </div>
+          <div className="filter-item-type1">
+            <label htmlFor="period">Period</label> <br />
+            <select name="period" id="period" defaultValue={"4"} onChange={filterApp}>
+              <option value="5" disabled>Period</option>
+              <option value="4">Default</option>
+              <option value="1">Today</option>
+              <option value="2">This week</option>
+              <option value="3">Last week</option>
+            </select>
+          </div>
+          <div className="filter-item-type1">
+            <label htmlFor="SpecifiedDate">Specified Date</label> <br />
+            <input type="date" id="SpecifiedDate" name="SpecifiedDate" onChange={filterApp} />
+          </div>
+          <div className="filter-item-type1">
+            <label htmlFor="SpecifiedTime">Specified Start Time</label> <br />
+            <input type="time" id="SpecifiedTime" name="SpecifiedTime" onChange={filterApp} />
+          </div>
+          <div className="filter-item-type1">
+            <label htmlFor="LevelOfImportance_f">Exam Type</label> <br />
+            <select name="LevelOfImportance" id="LevelOfImportance_f" defaultValue={8} onChange={filterApp}>
+              <option value={8} disabled>Exam Type</option>
+              <option value={9}>Reset</option>
+              <option value={3}>Assignment</option>
+              <option value={2}>Quiz</option>
+              <option value={1}>Mid Exam</option>
+              <option value={0}>End Exam</option>
+            </select>
+          </div>
+          <button className="me-15" onClick={() => window.location.reload()}>Clear Filters</button>
+        </div>
+      </section>
+
+      <section className="table-section">
+      <table>
+            <thead>
+                <tr>
+                    <th>Exam Hall</th>
+                    <th>Lecturer Email Address</th>
+                    <th>Number Of Students</th>
+                    <th>Batch</th>
+                    <th>Semester</th>
+                    <th>Subject</th>
+                    <th>Exam Type</th>
+                    <th>Date</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Academic Staff Member</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+          dataList.length === 0 ?
+            <div className="row mt-15 waiting">Loading <div className="loading">...</div></div> :
+            dataList.map(item => <Appointment item={item} key={item.id} stateListener={setStateListener} />)
+
+            }
+            </tbody>
+        </table>
+      </section>
+
+      </div>
+
+
+      <section>
+        <section className="modal new-modal hidden">
+          <New refreshApp={setRefreshData} />
+        </section>
+        <div className="block">
+          <section className="modal edit-modal hidden">
+            <Edit stateListener={stateListener} refreshApp={setRefreshData} />
+          </section>
+          <section className="modal delete-modal hidden">
+            <Delete stateListener={stateListener} refreshApp={setRefreshData} />
+          </section>
+        </div>
+      </section>
+    </main>
     </div>
   )
 }
