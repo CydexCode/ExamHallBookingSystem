5193f15e9e2309c78faa62be6707edad241656ee
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-06-07 10:47:01 +0530

    responsive frontend designs

--- ExamHallBooking.Presentation/ClientApp/src/Footer/Footer.js
+++ ExamHallBooking.Presentation/ClientApp/src/Footer/Footer.js
@@ -4,11 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <ul className='footerUL'>
-                <li>Instagram</li>
-                <li>Youtube</li>
-                <li>LinkedIn</li>
-            </ul>
+        
             <p className='footerText'>
                 &copy; 2024 Exam Booking System | All rights reserved | Terms of Service | Privacy
             </p>
